c5a4df7691ced3222fd4697273c5a63d12dcdd9c
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2017-11-15 15:16:29 +0100

    Eliminated unnecessary output

--- pagerank/src/main/java/eu/wdaqua/validation/FileExistValidator.java
+++ pagerank/src/main/java/eu/wdaqua/validation/FileExistValidator.java
@@ -7,7 +7,6 @@ import java.io.File;
 
 public class FileExistValidator implements IParameterValidator {
     public void validate(String name, String value) throws ParameterException {
-        System.out.println(value);
         File file =  new File(value);
         if(!file.exists() || file.isDirectory()) {
             throw new ParameterException("Parameter " + name + " which should represent a file that exists, does not exist.");

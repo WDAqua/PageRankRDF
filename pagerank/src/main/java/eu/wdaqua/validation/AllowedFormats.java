7b13036741f1778335b13f87f957c6453c88c87a
dd77474h <dennis.diefenbach@univ-st-etienne.fr>
2017-12-06 10:06:21 +0100

    Bug in allowed formats

--- pagerank/src/main/java/eu/wdaqua/validation/AllowedFormats.java
+++ pagerank/src/main/java/eu/wdaqua/validation/AllowedFormats.java
@@ -7,7 +7,7 @@ import com.beust.jcommander.ParameterException;
 public class AllowedFormats implements IParameterValidator {
     public void validate(String name, String value)
             throws ParameterException {
-        if (value.equals("tsv") || value.equals("nt")) {
+        if (!value.equals("tsv") && !value.equals("nt")) {
             throw new ParameterException("Parameter " + name + " should be either \"tsv\" or \"nt\" (found " + value +")");
         }
     }

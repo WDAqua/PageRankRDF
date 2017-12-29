8378f9eafd51b0eb0a7a3d0cfe1b269c9d9cf734
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2017-12-29 13:38:02 +0100

    Use binary search to identify literals

--- src/main/java/eu/wdaqua/pagerank/PageRankHDT.java
+++ src/main/java/eu/wdaqua/pagerank/PageRankHDT.java
@@ -1,12 +1,16 @@
 package eu.wdaqua.pagerank;
 
+import eu.wdaqua.pagerank.util.BinarySearch;
+
 import org.apache.jena.graph.Node;
+import org.rdfhdt.hdt.dictionary.Dictionary;
 import org.rdfhdt.hdt.enums.TripleComponentRole;
 import org.rdfhdt.hdt.hdt.HDT;
 import org.rdfhdt.hdt.hdt.HDTManager;
 import org.rdfhdt.hdt.triples.IteratorTripleID;
 import org.rdfhdt.hdt.triples.TripleID;
 
+import org.rdfhdt.hdt.util.string.CharSequenceComparator;
 import org.rdfhdt.hdtjena.NodeDictionary;
 
 import java.io.IOException;
@@ -23,7 +27,8 @@ public class PageRankHDT implements PageRank{
     private int[] numberOutgoing;
     private double[] pageRankScoresShared;
     private double[] pageRankScoresObjects;
-    List<Integer> indexesNonLiterals = new ArrayList<>();
+    private int start_literals_objects = -1;
+    private int end_literals_objects = -1;
 
     public PageRankHDT(String hdtDump){
         this.load(hdtDump);
@@ -65,20 +70,24 @@ public class PageRankHDT implements PageRank{
         //Save indeces which are not literals, don't do pagerank for literal only for not uris
         NodeDictionary nodeDictionary = new NodeDictionary(hdt.getDictionary());
         int numberNonLiterals = 0;
-        for (int id=1; id<=nObjects; id++){
-            boolean isLiteral = false;
-            if (id>nShared){
-                Node node = nodeDictionary.getNode(id,TripleComponentRole.OBJECT);
-                isLiteral = node.isLiteral();
-                if (!isLiteral) {
-                    indexesNonLiterals.add(id);
-                    numberNonLiterals++;
-                }
-            }
+        System.out.println(nObjects);
+        for (int i = 1; i<=nShared; i++){
+            System.out.println("DICT "+hdt.getDictionary().idToString(i,TripleComponentRole.OBJECT));
+        }
+        System.out.println("-----");
+        for (int i = nShared+1; i<=nObjects; i++){
+            System.out.println("DICT "+hdt.getDictionary().idToString(i,TripleComponentRole.OBJECT));
         }
 
+        start_literals_objects = BinarySearch.first(hdt.getDictionary(), nShared+1, nObjects);
+        System.out.println("start_literals_objects "+start_literals_objects);
+        end_literals_objects = BinarySearch.last(hdt.getDictionary(), nShared+1, nObjects, nObjects);
+        System.out.println("end_literals_objects "+end_literals_objects);
+        numberNonLiterals = nObjects-(end_literals_objects-start_literals_objects);
+        System.out.println("numberNonLiterals "+numberNonLiterals);
+
         pageRankScoresShared = new double[(int)hdt.getDictionary().getNshared()+1];
-        pageRankScoresObjects = new double[numberNonLiterals];
+        pageRankScoresObjects = new double[numberNonLiterals+1];
 
         //Initialize the start page rank scores
         for (int id=1; id<=nShared; id++){
@@ -110,29 +119,31 @@ public class PageRankHDT implements PageRank{
         System.err.println("Iteration...");
         for (int j = 0; j < numberOfIterations; j++) {
             System.err.print(j +" ");
-            for (int k=1; k<=nShared+numberNonLiterals; k++){
-                int id = k;
-                if (k>nShared){
-                    id = indexesNonLiterals.get(k-nShared-1);
-                }
-                TripleID root = new TripleID(0, 0,id);
-                IteratorTripleID incomingLinks= hdt.getTriples().search(root);
-                double pageRank = 1.0D - dampingFactor;
-                while (incomingLinks.hasNext()) {
-                    TripleID inLink = incomingLinks.next();
-                    Double pageRankIn = 0.0;
-                    if (inLink.getSubject()<=nShared){
-                        pageRankIn = pageRankScoresShared[inLink.getSubject()];
-                    } else {
-                        pageRankIn = startValue;
+            for (int id=1; id<=nObjects; id++){
+                if (!(id>=start_literals_objects && id<=end_literals_objects)) {
+                    TripleID root = new TripleID(0, 0, id);
+                    IteratorTripleID incomingLinks = hdt.getTriples().search(root);
+                    double pageRank = 1.0D - dampingFactor;
+                    while (incomingLinks.hasNext()) {
+                        TripleID inLink = incomingLinks.next();
+                        Double pageRankIn = 0.0;
+                        if (inLink.getSubject() <= nShared) {
+                            pageRankIn = pageRankScoresShared[inLink.getSubject()];
+                        } else {
+                            pageRankIn = startValue;
+                        }
+                        long numberOut = numberOutgoing[inLink.getSubject()];
+                        pageRank += dampingFactor * (pageRankIn / numberOut);
                     }
-                    long numberOut = numberOutgoing[inLink.getSubject()];
-                    pageRank += dampingFactor * (pageRankIn / numberOut);
-                }
-                if (id<=nShared){
-                    pageRankScoresShared[id]=pageRank;
-                } else {
-                    pageRankScoresObjects[k-nShared-1]=pageRank;
+                    if (id <= nShared) {
+                        pageRankScoresShared[id] = pageRank;
+                    } else {
+                        if (id < start_literals_objects){
+                            pageRankScoresObjects[id] = pageRank;
+                        } else {
+                            pageRankScoresObjects[id-(end_literals_objects-start_literals_objects)] = pageRank;
+                        }
+                    }
                 }
             }
         }
@@ -150,12 +161,19 @@ public class PageRankHDT implements PageRank{
             s.pageRank =  pageRankScoresShared[id];
             scores.add(s);
         }
-        for (int k=0; k<indexesNonLiterals.size(); k++){
-            int id = indexesNonLiterals.get(k);
-            PageRankScore s = new PageRankScore();
-            s.node = hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT).toString();
-            s.pageRank =  pageRankScoresObjects[k];
-            scores.add(s);
+        for (int id=nShared+1; id<=nObjects; id++){
+            if (id<start_literals_objects){
+                PageRankScore s = new PageRankScore();
+                s.node = hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT).toString();
+                s.pageRank =  pageRankScoresObjects[id];
+                scores.add(s);
+            }
+            if (id > end_literals_objects){
+                PageRankScore s = new PageRankScore();
+                s.node = hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT).toString();
+                s.pageRank =  pageRankScoresObjects[id-(end_literals_objects-start_literals_objects)];
+                scores.add(s);
+            }
         }
         for (int id=1; id<=(nSubjects-nShared); id++){
             PageRankScore s = new PageRankScore();
@@ -174,9 +192,13 @@ public class PageRankHDT implements PageRank{
         for (int id=1; id<=nShared; id++){
             writer.println(hdt.getDictionary().idToString(id,TripleComponentRole.SUBJECT) + "\t" + String.format("%.10f",pageRankScoresShared[id]));
         }
-        for (int k=0; k<indexesNonLiterals.size(); k++){
-            int id = indexesNonLiterals.get(k);
-            writer.println(hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT) + "\t" + String.format("%.10f",pageRankScoresObjects[k]));
+        for (int id=nShared+1; id<=nObjects; id++){
+            if (id<start_literals_objects){
+                writer.println(hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT) + "\t" + String.format("%.10f",pageRankScoresObjects[id]));
+            }
+            if (id > end_literals_objects){
+                writer.println(hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT) + "\t" + String.format("%.10f",pageRankScoresObjects[id-(end_literals_objects-start_literals_objects)]));
+            }
         }
         for (int id=1; id<=(nSubjects-nShared); id++){
             writer.println(hdt.getDictionary().idToString(id+nShared,TripleComponentRole.SUBJECT) + "\t" + String.format("%.10f",startValue));
@@ -190,9 +212,13 @@ public class PageRankHDT implements PageRank{
         for (int id=1; id<=nShared; id++){
             writer.println("<"+hdt.getDictionary().idToString(id,TripleComponentRole.SUBJECT)+"> <http://purl.org/voc/vrank#pagerank>\t \""+String.format("%.10f", pageRankScoresShared[id])+"\"^^<http://www.w3.org/2001/XMLSchema#float> .");
         }
-        for (int k=0; k<indexesNonLiterals.size(); k++){
-            int id = indexesNonLiterals.get(k);
-            writer.println("<"+hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT)+"> <http://purl.org/voc/vrank#pagerank>\t \""+String.format("%.10f",pageRankScoresObjects[k])+"\"^^<http://www.w3.org/2001/XMLSchema#float> .");
+        for (int id=nShared+1; id<=nObjects; id++){
+            if (id<start_literals_objects){
+                writer.println("<"+hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT)+"> <http://purl.org/voc/vrank#pagerank>\t \""+String.format("%.10f",pageRankScoresObjects[id])+"\"^^<http://www.w3.org/2001/XMLSchema#float> .");
+            }
+            if (id > end_literals_objects){
+                writer.println("<"+hdt.getDictionary().idToString(id,TripleComponentRole.OBJECT)+"> <http://purl.org/voc/vrank#pagerank>\t \""+String.format("%.10f",id-(end_literals_objects-start_literals_objects))+"\"^^<http://www.w3.org/2001/XMLSchema#float> .");
+            }
         }
         for (int id=1; id<=(nSubjects-nShared); id++){
             writer.println("<"+hdt.getDictionary().idToString(id+nShared,TripleComponentRole.SUBJECT)+"> <http://purl.org/voc/vrank#pagerank>\t \""+String.format("%.10f",startValue)+"\"^^<http://www.w3.org/2001/XMLSchema#float> .");
---
+++ src/main/java/eu/wdaqua/pagerank/util/BinarySearch.java
@@ -0,0 +1,64 @@
+package eu.wdaqua.pagerank.util;
+
+import org.rdfhdt.hdt.dictionary.Dictionary;
+import org.rdfhdt.hdt.enums.TripleComponentRole;
+import org.rdfhdt.hdt.util.string.CharSequenceComparator;
+
+// Small util to find literals in the dictionary using binary search
+
+public class BinarySearch {
+    private static CharSequenceComparator comparator = new CharSequenceComparator();
+
+    //FIRST occurrence of " in the dictionary between the indexes low and high, if not found return -1
+    public static int first(Dictionary dictionary, int low, int high) {
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
+            System.out.println("low "+low);
+            System.out.println("high "+high);
+            System.out.println("mid "+mid);
+
+
+            int c = -1;
+            if (mid!=1){
+                System.out.println(dictionary.idToString(mid - 1, TripleComponentRole.OBJECT).toString().subSequence(0, 1));
+                c = comparator.compare("\"", dictionary.idToString(mid - 1, TripleComponentRole.OBJECT).toString().subSequence(0, 1));
+            }
+            System.out.println("HERE");
+            System.out.println(dictionary.idToString(mid, TripleComponentRole.OBJECT));
+            int c2 = comparator.compare("\"", dictionary.idToString(mid, TripleComponentRole.OBJECT).toString().subSequence(0, 1));
+            System.out.println("c"+c);
+            System.out.println("c2 "+c2);
+            if ((mid == 1 || c < 0) && c2 == 0)
+                return mid;
+            else if (c > 0)
+                return first(dictionary, (mid + 1), high);
+            else
+                return first(dictionary, low, (mid - 1));
+        }
+        return -1;
+    }
+
+    //LAST occurrence of " in the dictionary between the indexes low and high, if not found return -1
+    public static int last(Dictionary dictionary, int low, int high, int n) {
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
+            System.out.println("low "+low);
+            System.out.println("high "+high);
+            System.out.println("mid "+mid);
+            int c = -1;
+            if (mid!=n) {
+                c = comparator.compare("\"", dictionary.idToString(mid + 1, TripleComponentRole.OBJECT).toString().subSequence(0, 1));
+            }
+            int c2 = comparator.compare("\"", dictionary.idToString(mid, TripleComponentRole.OBJECT).toString().subSequence(0, 1));
+            System.out.println("c"+c);
+            System.out.println("c2 "+c2);
+            if ((mid == n || c < 0) && c2 ==0)
+                return mid;
+            else if (c < 0)
+                return last(dictionary, low, (mid - 1), n);
+            else
+                return last(dictionary, (mid + 1), high, n);
+        }
+        return -1;
+    }
+}
